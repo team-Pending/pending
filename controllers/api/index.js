3af7741e9ab0a7e41a210eb796891c6cdc404baf
JCool221 <109809337+JCool221@users.noreply.github.com>
2022-12-21 21:28:51 -0500

    i think login works

---
+++ controllers/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+// const postRoutes = require('./postRoutes');
+
+router.use('/users', userRoutes);
+// router.use('/posts', postRoutes);
+
+module.exports = router;
