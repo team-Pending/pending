f3f9ddd2a8514912e38183d3cdee9512e3f60376
JCool221 <109809337+JCool221@users.noreply.github.com>
2022-12-21 20:31:22 -0500

    seeded some test users, server starts, connects to database and has data

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,16 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
+
+
+  const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
     host: 'localhost',
     dialect: 'mysql',
-    port: 3001,
+    port: 3306
   }
 );
 
---
+++ seeds/seed.js
@@ -0,0 +1,16 @@
+const sequelize = require('../config/connection');
+const User = require('../models/User');
+
+const userData = require('./userData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+  process.exit(0);
+};
+
+seedDatabase();
