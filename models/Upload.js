c73ed218490b2b22b26855d46683572dc71a95af
sasw <saswain96@gmail.com>
2022-12-28 15:11:06 -0600

    databases created

---
+++ models/Upload.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Upload extends Model {}
+
+Upload.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'user',
+            key: 'id',
+            unique: true
+        }
+    },
+    title: {
+        type: DataTypes.STRING
+    },
+    message: {
+        type: DataTypes.STRING
+    },
+    date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW
+    }
+},
+{
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'upload',
+});
+
+module.exports = Upload;
