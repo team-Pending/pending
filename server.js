814581226fb5ec99e4e501c72322c5ab2289be5d
JCool221 <109809337+JCool221@users.noreply.github.com>
2023-01-05 21:29:18 -0500

    update dotenv

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const uniqid = require('uniqid');
+require('dotenv').config();
+
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
