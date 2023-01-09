8c1c8a69b5dee08d068a06e5153e0c7ddb654aad
Ralakash <shawnkaralash@gmail.com>
2023-01-08 23:34:39 -0500

    adds some styling stuff

--- models/Note.js
+++ models/Note.js
@@ -3,42 +3,44 @@ const sequelize = require('../config/connection');
 
 class Note extends Model {}
 
-Note.init({
+Note.init(
+  {
     id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        autoIncrement: true,
-        //added for necessity to make work
-        primaryKey: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      //added for necessity to make work
+      primaryKey: true,
     },
     title: {
-        type: DataTypes.STRING
+      type: DataTypes.STRING,
     },
     description: {
-        type: DataTypes.STRING
+      type: DataTypes.STRING,
     },
     key: {
-        type: DataTypes.STRING
+      type: DataTypes.STRING,
     },
     date: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: DataTypes.NOW
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
     },
     user_id: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: 'user',
-            key: 'id'
-        }
-    }
-},
-{
-  sequelize,
-  timestamps: false,
-  freezeTableName: true,
-  underscored: true,
-  modelName: 'note',
-});
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'note',
+  }
+);
 
 module.exports = Note;
